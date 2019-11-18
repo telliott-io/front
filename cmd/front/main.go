b92f1700c8a457c4797ccc2412285a63560d1679
Tom Elliott <tom.w.elliott@gmail.com>
2019-11-17 22:35:34 -0500

    Remove bar test endpoint

--- cmd/front/main.go
+++ cmd/front/main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"html"
 	"log"
 	"net/http"
 )
@@ -12,10 +10,6 @@ func main() {
 	http.Handle("/styles/", http.StripPrefix("/styles/", fs))
 	views := http.FileServer(http.Dir("views/"))
 	http.Handle("/", http.StripPrefix("/", views))
-
-	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
 
 	log.Println("Listening on port 80")
 	log.Fatal(http.ListenAndServe(":80", nil))
