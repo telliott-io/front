5250d5fb31a140ccf8a6073a71e74c0ea5a2a171
Tom Elliott <tom.w.elliott@gmail.com>
2020-03-29 22:56:59 -0400

    Serve images separately.
    Serve each image from a single URL and modify the index template to load the image URL.

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,9 +1,12 @@
 package server
 
 import (
+	"encoding/base64"
+	"fmt"
 	"html/template"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
 	"github.com/opentracing/opentracing-go"
 	"github.com/prometheus/client_golang/prometheus"
@@ -29,20 +32,26 @@ type server struct {
 var (
 	indexServed = promauto.NewCounter(prometheus.CounterOpts{
 		Name: "front_index_served",
-		Help: "The total number index requests served",
+		Help: "The total number of index requests served",
+	})
+	imageServed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "front_image_served",
+		Help: "The total number of image requests served",
 	})
 )
 
 func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if strings.HasPrefix(r.URL.Path, "/image") {
+		s.serveImage(w, r)
+		return
+	}
+
 	indexServed.Inc()
 
 	tracer := opentracing.GlobalTracer()
 	span := tracer.StartSpan("serve_index")
 
-	span.SetTag("path", r.URL.Path)
-	span.SetTag("url", r.URL.String())
-	span.SetTag("referer", r.Referer())
-	span.SetTag("user-agent", r.UserAgent())
+	s.addSpanTags(span, r)
 
 	ctx := opentracing.ContextWithSpan(r.Context(), span)
 	defer span.Finish()
@@ -83,4 +92,45 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 	renderPageSpan.Finish()
+}
+
+func (s *server) serveImage(w http.ResponseWriter, r *http.Request) {
+	indexServed.Inc()
+
+	tracer := opentracing.GlobalTracer()
+	span := tracer.StartSpan("serve_image")
+
+	s.addSpanTags(span, r)
+
+	ctx := opentracing.ContextWithSpan(r.Context(), span)
+	defer span.Finish()
+
+	getProjectsSpan, _ := opentracing.StartSpanFromContext(ctx, "get-projects")
+	p, err := s.loader.GetProjects(ctx)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	getProjectsSpan.Finish()
+
+	renderImageSpan, _ := opentracing.StartSpanFromContext(ctx, "render-image")
+	for _, project := range p {
+		if fmt.Sprintf("/image/%v", project.Name) == r.URL.Path {
+			img, err := base64.StdEncoding.DecodeString(project.Image)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", project.ImageMimeType)
+			w.Write(img)
+		}
+	}
+	renderImageSpan.Finish()
+}
+
+func (s *server) addSpanTags(span opentracing.Span, r *http.Request) {
+	span.SetTag("path", r.URL.Path)
+	span.SetTag("url", r.URL.String())
+	span.SetTag("referer", r.Referer())
+	span.SetTag("user-agent", r.UserAgent())
 }
